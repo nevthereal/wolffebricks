2eb8e11cd27d367ad7047fddb140ed9caf38ca9d
Neville Brem <contact@nevillebrem.com>
2023-07-24 19:50:33 +0200

    customized checkout page

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const cors = require('cors')
 const stripe = require('stripe')(process.env.STRIPE_API_KEY)
 
 const app = express()
-app.use(cors())
+app.use(cors());
 app.use(express.static('public'))
 app.use(express.json())
 
-DOMAIN = process.env.STORE_DOMAIN
+
+const DOMAIN = process.env.STORE_DOMAIN; // Add the missing 'const' keyword
 
 app.post("/checkout", async (req, res) => {
   const items = req.body.items
@@ -25,9 +26,13 @@ app.post("/checkout", async (req, res) => {
     line_items: lineItems,
     mode: 'payment',
     allow_promotion_codes: true,
-    success_url: `${DOMAIN}/success`,
+    success_url: `${DOMAIN}/success?id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${DOMAIN}/cancel`,
-    customer_email: userEmail
+    customer_email: userEmail,
+    billing_address_collection: 'required',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CH', 'GB', 'DE'],
+    },  
   })
   
   res.json({ url: session.url })
