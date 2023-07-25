9adb95d53e34771a62e7ed9ddf1617f65e5d331b
Neville Brem <contact@nevillebrem.com>
2023-07-25 09:00:26 +0200

    finally added logic for success page

--- server/server.js
+++ server/server.js
@@ -1,41 +1,45 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const stripe = require('stripe')(process.env.STRIPE_API_KEY)
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
-const app = express()
+const app = express();
 app.use(cors());
-app.use(express.static('public'))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
 
-
-const DOMAIN = process.env.STORE_DOMAIN; // Add the missing 'const' keyword
+const DOMAIN = process.env.STORE_DOMAIN;
 
 app.post("/checkout", async (req, res) => {
-  const items = req.body.items
-  const userEmail = req.body.userEmail
-  let lineItems = []
+  const items = req.body.items;
+  const userEmail = req.body.userEmail;
+  let lineItems = [];
   items.forEach((item) => {
     lineItems.push({
       price: item.id,
-      quantity: item.quantity
-    })
-  })
+      quantity: item.quantity,
+    });
+  });
 
   const session = await stripe.checkout.sessions.create({
     line_items: lineItems,
-    mode: 'payment',
+    mode: "payment",
     allow_promotion_codes: true,
     success_url: `${DOMAIN}/success?id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${DOMAIN}/cancel`,
     customer_email: userEmail,
-    billing_address_collection: 'required',
+    billing_address_collection: "required",
     shipping_address_collection: {
-      allowed_countries: ['US', 'CH', 'GB', 'DE'],
-    },  
-  })
-  
-  res.json({ url: session.url })
-})
+      allowed_countries: ["US", "CH", "GB", "DE"],
+    },
+  });
 
-app.listen(4000, () => console.log("LISTENING ON PORT 4000"))
+  res.json({ url: session.url });
+});
+
+app.get("/order", async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.orderId);
+  res.json(session);
+});
+
+app.listen(4000, () => console.log("LISTENING ON PORT 4000"));
--- shop/src/pages/Success.jsx
+++ shop/src/pages/Success.jsx
@@ -1,18 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const Success = ({ auth }) => {
   const [user, loading] = useAuthState(auth);
+  const [orderInfo, setOrderInfo] = useState(null);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const orderId = urlParams.get("id");
+
+    fetch(`${import.meta.env.VITE_CHECKOUT_DOMAIN}/order?orderId=${orderId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((session) => {
+        return setOrderInfo(session);
+      })
+      .catch((error) =>
+        console.error("Error fetching order information:", error)
+      );
+  }, []);
 
   return (
-    <div>
-      Thank you for your order,{" "}
-      {user ? user.displayName : loading ? null : null}! You will get a mail
-      with the Instructions and Partlists soon!{" "}
-      <a href='/' className='font-bold'>
-        Back to home
-      </a>
-    </div>
+    <>
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
+          {orderInfo && user ? (
+            <>
+              <p>
+                Thank you for your order, {user.displayName}! You will get a
+                mail with the Instructions and Partlists soon!
+              </p>
+              <div className='my-4'>
+                <h1 className='text-2xl font-bold'>Your order details:</h1>
+                <ul>
+                  <li>
+                    Subtotal:{" "}
+                    <span className='font-semibold'>
+                      {orderInfo.amount_total / 100} CHF
+                    </span>
+                  </li>
+                  <li>
+                    Payment status:{" "}
+                    <span className='font-semibold'>{orderInfo.status}</span>
+                  </li>
+                  <li>
+                    Order ID:{" "}
+                    <span className='font-semibold'>
+                      {orderInfo.payment_intent}
+                    </span>
+                  </li>
+                </ul>
+                <div className='my-2'>
+                  <h1 className='text-lg font-bold'>Customer Details:</h1>
+                  <ul className='font-semibold'>
+                    <li>
+                      <span className='font-normal'>Email:</span>{" "}
+                      {orderInfo.customer_details.email}
+                    </li>
+                    <li>
+                      <span className='font-normal'>Name:</span>{" "}
+                      {orderInfo.customer_details.name}
+                    </li>
+                    <li>
+                      {orderInfo.customer_details.address.line1}{" "}
+                      {orderInfo.customer_details.address.line2}
+                    </li>
+                    <li>
+                      {orderInfo.customer_details.address.postal_code}{" "}
+                      {orderInfo.customer_details.address.city},{" "}
+                      {orderInfo.customer_details.address.country}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <button className='font-bold border border-gray-400 p-2 rounded-lg py-1 px-2 text-xl hover:scale-105 duration-200'>
+                <a href='/' className='font-bold'>
+                  Back to home
+                </a>
+              </button>
+            </>
+          ) : (
+            <p>Loading ...</p>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
