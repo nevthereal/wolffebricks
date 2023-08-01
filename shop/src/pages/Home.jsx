0a4ab65108daee74b48cea13eb74160ab7a563c2
Neville Brem <contact@nevillebrem.com>
2023-08-01 16:15:21 +0200

    typo fix

--- shop/src/pages/Home.jsx
+++ shop/src/pages/Home.jsx
@@ -8,35 +8,35 @@ const Home = () => {
   return (
     <div>
       <div
-        id='header'
-        className='text-center bg-no-repeat bg-center bg-cover w-full bg-white'
+        id="header"
+        className="text-center bg-no-repeat bg-center bg-cover w-full bg-white"
       >
-        <div className='w-full bg-gray-800/25 dark:bg-black/70 h-[85vh] md:h-[90vh]'>
-          <div className='absolute top-[40vh] text-4xl md:text-6xl font-black text-white bg-cover w-full h-40'>
-            <h1 className='uppercase'>Wolffe Brix</h1>
-            <p className='text-base md:text-lg tracking-wider my-4'>
+        <div className="w-full bg-gray-800/25 dark:bg-black/70 h-[85vh] md:h-[90vh]">
+          <div className="absolute top-[40vh] text-4xl md:text-6xl font-black text-white bg-cover w-full h-40">
+            <h1 className="uppercase">Wolffe Bricks</h1>
+            <p className="text-base md:text-lg tracking-wider my-4">
               LEGO® MOCs from Switzerland!
             </p>
-            <div className='text-base bg-orange-400 border-2 border-orange-600 w-fit mt-6 mb-2 md:mx-auto mx-6 my-8 p-3 rounded-2xl'>
-              <h2 className='text-2xl'>Big Change!</h2>
-              <p className='font-semibold'>
+            <div className="text-base bg-orange-400 border-2 border-orange-600 w-fit mt-6 mb-2 md:mx-auto mx-6 my-8 p-3 rounded-2xl">
+              <h2 className="text-2xl">Big Change!</h2>
+              <p className="font-semibold">
                 We now switched from only selling instructions to selling full
                 sets! We currently only ship to few countries, but plan to ship
                 to more soon.
               </p>
             </div>
-            <a href='#featured'>
+            <a href="#featured">
               <FontAwesomeIcon
-                className='text-3xl md:text-4xl hover:scale-110 duration-200'
+                className="text-3xl md:text-4xl hover:scale-110 duration-200"
                 icon={faChevronDown}
               />
             </a>
           </div>
         </div>
       </div>
-      <div id='featured'>
-        <h1 className='text-3xl font-bold uppercase my-4'>Featured:</h1>
-        <div className='flex justify-center gap-4 flex-wrap'>
+      <div id="featured">
+        <h1 className="text-3xl font-bold uppercase my-4">Featured:</h1>
+        <div className="flex justify-center gap-4 flex-wrap">
           <Panel
             link={`/products/${products[2].link}`}
             id={products[2].id}
@@ -60,7 +60,7 @@ const Home = () => {
           />
         </div>
       </div>
-      <div className='py-8'></div>
+      <div className="py-8"></div>
     </div>
   );
 };
--- shop/src/pages/Info.jsx
+++ shop/src/pages/Info.jsx
@@ -13,11 +13,11 @@ import {
 
 const Info = () => {
   return (
-    <div className='py-8 px-10 md:px-60'>
-      <h1 className='text-4xl md:text-6xl font-black uppercase my-8'>Info</h1>
-      <div className='my-6'>
-        <h1 className='text-xl md:text-2xl font-bold'>About us</h1>
-        <p className='md:text-xl'>
+    <div className="py-8 px-10 md:px-60">
+      <h1 className="text-4xl md:text-6xl font-black uppercase my-8">Info</h1>
+      <div className="my-6">
+        <h1 className="text-xl md:text-2xl font-bold">About us</h1>
+        <p className="md:text-xl">
           Me (Neville Brem) and William Tang are two students from Switzerland,
           who love to build and design LEGO models. In Summer 2022, we came up
           with the idea to sell our designs. William designed most of the sets,
@@ -25,17 +25,17 @@ const Info = () => {
           now sell full sets, instead of intructions only. Currently, we only
           ship to Europe, the US, Canada, Korea and Japan. <br />
           With purchasing our products, you do a favor to the climate.{" "}
-          <a href='https://climate.stripe.com/qDvMjm' className='font-bold'>
+          <a href="https://climate.stripe.com/qDvMjm" className="font-bold">
             Carbon Removal
           </a>{" "}
           is funded by 0.5% of each purchase.
         </p>
       </div>
-      <div className='my-6'>
-        <h1 className='text-xl md:text-2xl font-bold'>Checkout Process</h1>
-        <p className='md:text-xl'>
+      <div className="my-6">
+        <h1 className="text-xl md:text-2xl font-bold">Checkout Process</h1>
+        <p className="md:text-xl">
           Your Checkouts are securely managed by{" "}
-          <a href='https://stripe.com' className='font-bold'>
+          <a href="https://stripe.com" className="font-bold">
             Stripe
           </a>
           . We will then send the instruction(s) and a partlist file(s) to your
@@ -43,12 +43,12 @@ const Info = () => {
           Account or your email, which you need to confirm first.
         </p>
       </div>
-      <div className='my-6'>
-        <h1 className='text-xl md:text-2xl font-bold'>Payment methods</h1>
-        <p className='md:text-xl'>
+      <div className="my-6">
+        <h1 className="text-xl md:text-2xl font-bold">Payment methods</h1>
+        <p className="md:text-xl">
           Our payments are in CHF (Swiss Franc), because we are a
           Switzerland-based company. We accept the following credit cards:
-          <ul className='flex justify-center text-4xl gap-4 my-2'>
+          <ul className="flex justify-center text-4xl gap-4 my-2">
             <li>
               <FontAwesomeIcon icon={faCcVisa} />
             </li>
@@ -69,7 +69,7 @@ const Info = () => {
             </li>
           </ul>
           And following wallets:
-          <ul className='flex justify-center text-4xl gap-4 my-2'>
+          <ul className="flex justify-center text-4xl gap-4 my-2">
             <li>
               <FontAwesomeIcon icon={faApplePay} />
             </li>
@@ -80,42 +80,42 @@ const Info = () => {
         </p>
       </div>
 
-      <h1 className='text-4xl md:text-6xl font-black uppercase my-8'>
+      <h1 className="text-4xl md:text-6xl font-black uppercase my-8">
         Contact
       </h1>
-      <p className='md:text-xl my-1'>
+      <p className="md:text-xl my-1">
         Send us an Email:{" "}
-        <a className='font-bold' href='mailto:support@wolffebricks.store'>
+        <a className="font-bold" href="mailto:support@wolffebricks.store">
           support@willsbrix.com
         </a>
       </p>
-      <p className='md:text-xl my-1'>
+      <p className="md:text-xl my-1">
         Send us a direct message on Twitter:{" "}
         <a
-          className='font-bold'
-          target='_blank'
-          href='https://twitter.com/wolffe_bricks'
+          className="font-bold"
+          target="_blank"
+          href="https://twitter.com/wolffe_bricks"
         >
-          @wolffe-bricks
+          @wolffe_bricks
         </a>
       </p>
-      <p className='md:text-xl my-1'>
-        Follow us on Instagran:{" "}
+      <p className="md:text-xl my-1">
+        Follow us on Instagram:{" "}
         <a
-          className='font-bold'
-          target='_blank'
-          href='https://instagram.com/wolffebricks_official'
+          className="font-bold"
+          target="_blank"
+          href="https://instagram.com/wolffebricks_official"
         >
           @wolffebricks_official
         </a>
       </p>
 
-      <div className='flex gap-2 justify-center mt-8'>
-        <a href='/terms-of-service' className='font-bold'>
+      <div className="flex gap-2 justify-center mt-8">
+        <a href="/terms-of-service" className="font-bold">
           Terms of Service
         </a>
         <p> • </p>
-        <a href='/privacy-policy' className='font-bold'>
+        <a href="/privacy-policy" className="font-bold">
           Privacy Policy
         </a>
       </div>
